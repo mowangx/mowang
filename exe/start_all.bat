8856fb2c942fd8b8e2505d077e59d588bfa45b6e
mowangx <36142663+mowangx@users.noreply.github.com>
2018-08-02 15:12:00 +0800

    # add start_all script

---
+++ exe/start_all.bat
@@ -0,0 +1,23 @@
+cd game_manager
+for /l %%i in (1,1,1) do (
+	start game_manager.exe %%i
+	ping 127.0.0.1 -n 1 -w 1000 > nul
+)
+
+cd ../db_manager
+for /l %%i in (1,1,2) do (
+	start db_manager.exe %%i
+	ping 127.0.0.1 -n 1 -w 1000 > nul
+)
+
+cd ../game
+for /l %%i in (1,1,3) do (
+	start game.exe %%i
+	ping 127.0.0.1 -n 1 -w 1000 > nul
+)
+
+cd ../gate
+for /l %%i in (1,1,3) do (
+	start gate.exe %%i
+	ping 127.0.0.1 -n 1 -w 1000 > nul
+)
